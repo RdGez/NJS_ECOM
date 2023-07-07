adb0101c4d543956022aab7b9e2bc181beb82749
Oscar Rodríguez <RdGez.Oscar@gmail.com>
2023-07-06 18:54:41 -0600

    feat(context): Modify context middleware

--- src/config/GraphQL/context.ts
+++ src/config/GraphQL/context.ts
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken';
-import { IJwtPayload } from '../../shared/interfaces/jwt.interface';
+import jwt from "jsonwebtoken";
+import { IJwtPayload } from "../../shared/interfaces/jwt.interface";
 
 export interface IContext {
   id: string;
@@ -7,17 +7,19 @@ export interface IContext {
 }
 
 export const context = async ({ req }: IContext) => {
-  const token = req.headers.token || '';
+  const token = req.headers.token || "";
   if (!token) return { user: null };
 
-  try { 
-    const { id } = jwt.verify(token, `${process.env.JWT_SECRET}`) as IJwtPayload;
+  try {
+    const { id } = jwt.verify(token, `${process.env.JWT_SECRET}`) as IJwtPayload || {};
     return {
       ...context,
       id,
     };
   } catch (error) {
-    console.error(error);
-    return { user: null };
+    return {
+      ...context,
+      id: "Not-Authenticated",
+    };
   }
 };
